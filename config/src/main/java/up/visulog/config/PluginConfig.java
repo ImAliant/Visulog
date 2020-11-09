d5be5f220a625e23f9bc67b5f6d96635ee71ee35
Basile HURET <basile@novellmania.com>
2020-11-09 11:09:36 +0100

    Je n'ai pas pu résoudre le TODO mais j'ai commenté les classes et ai un peu compris leur fonctionnement

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -1,5 +1,18 @@
 package up.visulog.config;
 
+//Un PluginConfig va servir en tant que valeur d'un objet de type Map.
+//
+//Une Map contient une clé et une valeur
 // TODO: define what this type should be (probably a Map: settingKey -> settingValue)
-public interface PluginConfig {
+public class PluginConfig {
+
+    //Attributs
+
+    private int val;
+
+
+
+
+    //Constructeur
+    public PluginConfig()
 }
