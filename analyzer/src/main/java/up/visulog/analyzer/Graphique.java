23cbbe3fbdfd9e190086ef04a2bdf8d1639eb150
TRAN marilyn <marilyn24@hotmail.fr>
2020-11-26 19:17:29 +0100

    Update analyzer/src/main/java/up/visulog/analyzer/Graphique.java

--- analyzer/src/main/java/up/visulog/analyzer/Graphique.java
+++ analyzer/src/main/java/up/visulog/analyzer/Graphique.java
@@ -1,20 +1,20 @@
-
 public class Graphique {
 	String nom_graphique;
-	
+
 	 public Graphique (String n) {
 		 this.nom_graphique=n;
 	 }
-	 
+
 	 public String getNom_graphique(){
 		 return this.nom_graphique;
 	 }
-	 
-	 public void ouvrir_canvas() {
-		 System.out.println("<canvas id="+this.nom_graphique+"></canvas>");
+
+	 public String ouvrir_canvas() {
+		 return "<canvas id="+this.nom_graphique+"></canvas>";
 	 }
-	 
-	 public void import_chartJS() {
-		 System.out.println("<script src=\'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js'>");
+
+	 public String import_chartJS() {//doit être dans le head
+		 return "<script src="+"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js>"+"</script>";
+		  
 	 }
 }
