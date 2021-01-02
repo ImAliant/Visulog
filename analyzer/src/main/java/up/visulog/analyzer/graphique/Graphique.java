743744546a14b777aae02695edb8dbb3daf00133
benakli <william.benakli.4.2@gmail.com>
2021-01-02 03:57:39 +0100

    Correction du bug d'affichage sur les graphiques

--- analyzer/src/main/java/up/visulog/analyzer/graphique/Graphique.java
+++ analyzer/src/main/java/up/visulog/analyzer/graphique/Graphique.java
@@ -20,37 +20,44 @@ public class Graphique{
          return "<canvas id='"+this.nom_graphique+"'></canvas>";
      } 
  
-     public String import_chartJS() {//doit ï¿½tre dans le head
+     public String import_chartJS() {
          return "<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js'></script>";
           
      }
      
     public String toGraph(String type, ArrayList<String> noms, ArrayList<Integer> nombreCommit) {
         Random r = new Random();
-        
-        String result = "function changeGraphique(){ ";
-                result+="var context = document.getElementById('"+this.nom_graphique+"').getContext('2d'); ";
-                result+="var data = {";
-                result+="labels : [";
-                for (int i =0;i<noms.size()-1;i++) {
-                    result+="'";
-                    result+=noms.get(i);
-                    result+="',";
-                }
-                result+="'"+noms.get(noms.size()-1)+"'],";
-                result+="datasets : [{ data : [";
-                for (int i =0;i<nombreCommit.size()-1;i++) {
-                    result+=nombreCommit.get(i)+",";
-                }
-                result+=nombreCommit.get(nombreCommit.size()-1)+"],";
-                result+="backgroundColor :  [ ";
-                for(int i =0;i<noms.size()-1;i++)
-                    result+="'rgba(" + r.nextInt(255) +","+ r.nextInt(255) +","+ r.nextInt(255) +",1)', ";
-                result+="]}]};";        
-                result += "var type = document.getElementById('graphiqueListe').value;";
-                result += "var config = { type : type, data : data};";
-                result += "var graph = new Chart (context, config);";
-            result += " };";
+        String result = "";
+        		 result += "var graph;";
+        		 result += "var config;";
+        		 result += "function changeGraphique(){";
+        		 result +="";
+        		 result +="if(graph == null){ ";
+	                result+="var context = document.getElementById('"+this.nom_graphique+"').getContext('2d'); ";
+	                result+="var data = {";
+	                result+="labels : [";
+	                for (int i =0;i<noms.size()-1;i++) {
+	                    result+="'";
+	                    result+=noms.get(i);
+	                    result+="',";
+	                }
+	                result+="'"+noms.get(noms.size()-1)+"'],";
+	                result+="datasets : [{ data : [";
+	                for (int i =0;i<nombreCommit.size()-1;i++)result+=nombreCommit.get(i)+",";
+	                result+=nombreCommit.get(nombreCommit.size()-1)+"],";
+	                result+="backgroundColor :  [ ";
+	                for(int i =0;i<noms.size()-1;i++)
+	                    result+="'rgba(" + r.nextInt(255) +","+ r.nextInt(255) +","+ r.nextInt(255) +",1)', ";
+	                result+="]}]};";        
+	                result += "var type = document.getElementById('graphiqueListe').value;";
+	                result += "config = { type : type, data : data};";
+	                result += "graph = new Chart (context, config);";
+            result += "}else{ ";
+            result +=  "graph.destroy();";
+            result += " graph = null;";
+            result +=  "changeGraphique();";
+            result +=  "}";
+            result +=  "} ";
     return result;
 }
     
